68c87922a8a72dec30a72dc44837dd68ce62d36f
asayda01 <56931334+asayda01@users.noreply.github.com>
2020-01-02 15:55:58 +0000

    updated main()

--- cities.py
+++ cities.py
@@ -25,11 +25,10 @@ def print_cities(road_map):
     list4=[]
     for i in range(0,len(road_map)):
         list4.append([road_map[i][0],road_map[i][1],(round((float(road_map[i][2])),2)),(round(float(road_map[i][3]),2))])
-    return list4
+    print(*list4,sep="\n")
 
 def compute_total_distance(road_map):
     total_dist1 = 0.0
-    city_x1,city_y1,city_x2,city_y2=0,0,0,0
     for i in range(0,len(road_map)):
         city_x1 = float(road_map[i][2])
         city_y1 = float(road_map[i][3])
@@ -45,7 +44,7 @@ def swap_cities(road_map,index1,index2):
     new_road_map[index1] = new_value_2
     new_road_map[index2] = new_value_1
     new_total_distance = compute_total_distance(new_road_map)
-    return new_road_map,new_total_distance
+    return (new_road_map,new_total_distance)
 
 def shift_cities(road_map):
     new_shifted_road_map=[road_map[-1]] + road_map[:-1]
@@ -70,7 +69,7 @@ def print_map(road_map):
     road_map_last = find_best_cycle(road_map)[0]
     total_dist =0
     list5 = []
-    for i in range(len(road_map_last) - 1):
+    for i in range(0,len(road_map_last) - 1):
         new_city_x3 = float(road_map_last[i][2])
         new_city_y3 = float(road_map_last[i][3])
         new_city_x4 = float(road_map_last[(i + 1) % len(road_map_last)][2])
@@ -81,15 +80,35 @@ def print_map(road_map):
         new_line1 = " From City : " + road_map_last[i][1] + " ( " + " State : " +  road_map_last[i][0] + " ) to" +" City : "+ road_map_last[(i + 1) % len(road_map_last)][1] + " ( "  + " State : " + road_map_last[(i + 1) % len(road_map_last)][0]  + " ) " + " Distance : " + str(round(new_dist1, 2))
         list5.append(new_line1)
         list5.append("Total distance : ---> " + str(round(total_dist, 2)))
-    return (list5)
+    print(*list5,sep="\n")
 
 def main():
+    file_name = "city-data.txt"
+    #print("-------------------file-name-yazdik------------------------------------------------------------------")
+    road_map= read_cities(file_name)
+    #print("----------read-cities-(file_name)-yazdik--------------------------------------------------------------------------")
+    print_cities(road_map)
+    #print("--------------print_cities(road_map)---fonksyionunu-cagirdik---------------------------------------------------------------------")
+    print(compute_total_distance(road_map))
+    #print("----------------print---compute_total_distance(road_map)------------------------------------------------------------------")
+    index1 = int(len(road_map) * random.random())
+    index2 = int(len(road_map) * random.random())
+    #print("-------------------index-1-2-yazdik-----------------------------------------------------------------")
+    swap_cities(road_map, index1, index2)
+    #print(swap_cities(road_map, index1, index2))
+    #print("-------------------swap-cities-yazdik------------------------------------------------------------------")
+    #print(*shift_cities(road_map),sep="\n")
+    shift_cities(road_map)
+    #print("-----------print-shift-cities--------------------------------------------------------------------------------------------------")
+    print_map(road_map)
+    #print("------------------print-road-map------------------------------------------------------------------------------------------------------------------")
+    #print("--------------------------------------------------------------------------------------------------------------------------------------")
+    print("--------------------------------------------------------------------------------------------------------------------------------------")
 
-    """
+"""
     Reads in, and prints out, the city data, then creates the "best"
     cycle and prints it out.
-    """
-    pass
+"""
 
 if __name__ == "__main__": #keep this in
     main()
